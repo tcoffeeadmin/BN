b387f20ee4822342dfcd4e73870201ab510b011f
midhun <projects@tcoffee.in>
2018-12-08 09:58:13 +0530

    07-12-2018

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form>
+<table>
+	<tr>
+		<td>Username</td>
+		<td><input type="text" name="uname"></td>
+	</tr>
+	<tr>
+		<td>Password</td>
+		<td><input type="text" name="password"></td>
+	</tr>
+	<tr>
+		<td><input type="submit" name="Login"></td>
+	</tr>
+</table>
+</form>
+</body>
+</html>
